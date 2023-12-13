799015d68a47af1498a5d6d90609f46861930a86
IhsanKhawaja <113303686+IhsanKhawaja@users.noreply.github.com>
2023-12-13 09:21:52 -0600

    added inanimate object

--- InanimateObject.java
+++ InanimateObject.java
@@ -1,8 +1,29 @@
 import mayflower.Actor;
+import mayflower.MayflowerImage;
+import mayflower.Timer;
 
 public class InanimateObject extends Actor {
+    private Animation animation;
+    private Timer animationTimer;
 
-    public void act() {
+    public InanimateObject(String[] frame){
+        setAnimation(new Animation(50, frame));
+    }
 
+    public Animation getAnimation() {
+        return animation;
+    }
+
+    public void setAnimation(Animation a){
+        animation = a;
+        animationTimer = new Timer(a.getFrameRate());
+    }
+
+    public void act(){
+        if(animationTimer.isDone() && animation != null){
+            MayflowerImage img = new MayflowerImage(animation.getNextFrame());
+            setImage(img);
+            animationTimer.reset();
+        }
     }
 }
